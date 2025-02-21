804c1d07097c2d7fdeeb9d0f47c6de28314e3756
SAMWE5 <sindayigayasamuel38@gmail.com>
2025-02-21 10:09:10 +0200

    Add files via upload

---
+++ oop assignment/oop assignment/example2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int age = 30;
+    float pi = 3.14;
+    double num = 3.1415926535;
+    char letter = 'A';
+    bool isHappy = true;
+    string name = "Alice";
+
+    // Output the values
+    cout << "Age: " << age << endl;
+    cout << "Pi (float): " << pi << endl;
+    cout << "Pi (double): " << num << endl;
+    cout << "Letter: " << letter << endl;
+    cout << "Is Happy? " << (isHappy ? "Yes" : "No") << endl;
+    cout << "Name: " << name << endl;
+
+    return 0;
+}
+
